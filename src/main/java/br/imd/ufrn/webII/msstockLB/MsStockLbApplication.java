ccc04409dde6916b43d2ee6c1ec75e828eb8633d
Luis <luiseduardofc1500@gmail.com>
2024-03-28 23:11:31 -0300

    initialize the project

---
+++ src/main/java/br/imd/ufrn/webII/msstockLB/MsStockLbApplication.java
@@ -0,0 +1,13 @@
+package br.imd.ufrn.webII.msstockLB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStockLbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsStockLbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/imd/ufrn/webII/msstockLB/MsStockLbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.imd.ufrn.webII.msstockLB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStockLbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
